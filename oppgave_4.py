c36b716c20ec80b37a399af499892388a9db6613
Celine Pedersen <celinetp@uia.no>
2026-09-21 23:08:45 +0200

    Refactor poeng calculations in oppgave_4.py

--- oppgave_4.py
+++ oppgave_4.py
@@ -3,19 +3,15 @@ poeng = 0
 # Jeg tror poeng blir: 10
 poeng = poeng + 10
 print(poeng)
-
 # Jeg tror poeng blir: 35
 poeng += 25
 print(poeng)
-
 # Jeg tror poeng blir: 30
 poeng -= 5
 print(poeng)
-
 # Jeg tror poeng blir: 60
 poeng *= 2
 print(poeng)
-
 # Jeg tror poeng blir: 15
 poeng /= 4
 print(poeng)
